11e60c7b3ef03d12285a8adc6c48b4cf54d7fe28
ksaikiran950 <ksaikiran950@gmail.com>
2025-10-08 23:15:58 +0530

    do changes

--- Python/Py_Tasks/Calci.py
+++ Python/Py_Tasks/Calci.py
@@ -1,9 +1,10 @@
 class Calci:
+
+    print("Choose operation: ")
+    print("1. Add\n2. Sub\n3. Mul\n4. Div\n5. Rem\n6. Exit\n7. Show Name")
+    
     def __init__(self, name):
         self.name = name  
-
-    def get_name(self):
-        return self.name
 
     def add(self, a, b):
         return a + b
@@ -19,41 +20,37 @@ class Calci:
 
     def rem(self, a, b):
         return a % b
-    
-
-name= input("enter your name: ")
-s1=Calci(name)
 
 
-print(f'Hi {s1.get_name}  !   Welcome')
+name = input("Enter your name: ")
 
-print("Choose operation: ")
-print("1. Add\n2. Sub\n3. Mul\n4. Div\n5. Rem\n6. Exit\n7. Show Name")
+s1 = Calci(name)
+
+print(f"\nHello {s1.name}! Welcome ")
 
 while True:
-    n = int(input("\nEnter your option: "))
-
-    if n == 6:
-        print(f"Exit")
-        break
-
-    elif n == 7:
-        print(f"Name: {s1.get_name()}")
-        continue
-
-    a, b = map(int, input("Enter two values : ").split())
+    n = int(input("\nEnter your option: "))   
 
     if n == 1:
-        print("Result:", s1.add(a, b))
+        a, b = map(int, input("Enter two values : ").split())
+        print(s1.add(a, b))
     elif n == 2:
-        print("Result:", s1.sub(a, b))
+        a, b = map(int, input("Enter two values : ").split())
+        print(s1.sub(a, b))
     elif n == 3:
-        print("Result:", s1.mul(a, b))
+        a, b = map(int, input("Enter two values : ").split())
+        print(s1.mul(a, b))
     elif n == 4:
-        print("Result:", s1.div(a, b))
+        a, b = map(int, input("Enter two values : ").split())
+        print(s1.div(a, b))
     elif n == 5:
-        print("Result:", s1.rem(a, b))
+        a, b = map(int, input("Enter two values : ").split())
+        print(s1.rem(a, b))    
+    elif n == 6:
+        print(f"Exit")
+        break
+    elif n == 7:
+        print(f"Name: {s1.name}")
+        continue
     else:
         print("Invalid choice. Please try again.")
-
-
