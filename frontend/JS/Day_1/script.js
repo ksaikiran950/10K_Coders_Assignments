fc948c101ce25356c2728877791c1422ce3d2a15
K SaiKiran <ksaikiran950@gmail.com>
2025-11-13 11:11:00 +0530

    js

---
+++ frontend/JS/Day_1/script.js
@@ -0,0 +1,6 @@
+document.write("Sample inline Script....")
+console.log("Sample external Script....")
+alert("Sample external Script....")
+function showMessage() {
+    alert("Button clicked! This is a message from the external script.");
+}
