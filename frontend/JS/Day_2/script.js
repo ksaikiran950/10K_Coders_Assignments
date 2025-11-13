f668cc7c86e16c5fd4185283a5f1d2e419f084e8
K SaiKiran <ksaikiran950@gmail.com>
2025-11-13 23:26:20 +0530

    scopes

---
+++ frontend/JS/Day_2/script.js
@@ -0,0 +1,27 @@
+// x=10;
+// function x(){}
+// console.log(x)
+
+// var a=10;
+// {
+//     var a=20;
+//     let b=30;
+//     console.log(a,b)
+// }
+// console.log(a)
+
+// const num=5;
+// num=10;
+// console.log(num)
+
+// const person ={ name:"John"}
+// person.name ="Alice";
+// console.log(person)
+
+
+let x=100;
+function demo(){
+    console.log(x);
+    let x=200;
+}
+demo();
